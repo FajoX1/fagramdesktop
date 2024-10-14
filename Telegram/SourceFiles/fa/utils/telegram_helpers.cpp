4093f62aa955a917249e97dba3b2ad05b3b62ce2
FajoX <146753912+FajoX1@users.noreply.github.com>
2024-10-14 18:41:08 +0300

    Update telegram_helpers.cpp

--- Telegram/SourceFiles/fa/utils/telegram_helpers.cpp
+++ Telegram/SourceFiles/fa/utils/telegram_helpers.cpp
@@ -11,6 +11,8 @@ https://github.com/fajox1/fagramdesktop/blob/master/LEGAL
 #include <QtCore/QJsonArray>
 #include <QtNetwork/QNetworkRequest>
 #include <QtCore/QEventLoop>
+#include <QDir>
+#include <QFile>
 
 std::unordered_set<ID> fagram_channels;
 std::unordered_set<ID> fagram_devs;
