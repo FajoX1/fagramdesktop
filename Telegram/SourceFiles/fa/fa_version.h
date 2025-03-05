1387fc16a76ee8dfd27bbf312aea5392af4d3ef0
FajoX <146753912+FajoX1@users.noreply.github.com>
2025-03-05 22:03:35 +0000

    v1.1.7 -> v1.1.8

--- Telegram/SourceFiles/fa/fa_version.h
+++ Telegram/SourceFiles/fa/fa_version.h
@@ -8,7 +8,7 @@ https://github.com/fajox1/fagramdesktop/blob/master/LEGAL
 
 #pragma once
 
-constexpr auto AppFAVersion = 1000017;
-constexpr auto AppFAVersionStr = "1.1.7";
-constexpr auto AppTGDVersion = "5.11.2";
+constexpr auto AppFAVersion = 1000018;
+constexpr auto AppFAVersionStr = "1.1.8";
+constexpr auto AppTGDVersion = "5.11.1";
 constexpr auto AppFABetaVersion = false;
